27e080e2a5757e8e38a4c5c52ecfe6e4329197b0
hazrafhazwan <muhdhazraf@gmail.com>
2020-12-13 15:05:32 +0000

    5.1 Client

---
+++ client.py
@@ -0,0 +1,15 @@
+import socket
+
+s = socket.socket()
+
+port = 8888
+
+s.connect(('172.20.10.13', port))
+
+data = s.recv(1024)
+
+s.send(b'Hi, saya client. Terima Kasih!');
+
+print (data)
+
+s.close()
